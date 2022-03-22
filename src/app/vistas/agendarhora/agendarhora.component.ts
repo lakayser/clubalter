12759a9d513b88820a017fb9a43f7088cfd33981
Matias Guerrero <matiguachiperro@gmail.com>
2022-03-22 10:18:41 -0300

    a

--- src/app/vistas/agendarhora/agendarhora.component.ts
+++ src/app/vistas/agendarhora/agendarhora.component.ts
@@ -3,22 +3,33 @@ import { HoraDisponible } from 'src/app/modelos/horadisponible';
 import { HoradisponibleService } from 'src/app/servicios/horadisponible.service';
 import { HorariosService } from 'src/app/servicios/horarios.service';
 import { Router } from '@angular/router';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm } from '@angular/forms';
 import * as moment from 'moment';
 import { Observable } from 'rxjs';
+import { UsuariosService } from 'src/app/servicios/usuarios.service';
 @Component({
   selector: 'app-agendarhora',
   templateUrl: './agendarhora.component.html',
   styleUrls: ['./agendarhora.component.css']
 })
 export class AgendarhoraComponent implements OnInit {
+  
 
-  constructor(private route:Router, public horadisponibleService: HoradisponibleService, public horariosService:HorariosService) { }
+  constructor(private route:Router, public horadisponibleService: HoradisponibleService, public horariosService:HorariosService, public usuariosService:UsuariosService) { }
   public hora: Array<HoraDisponible> = [];
+ 
 
   ngOnInit(): void {
+    
+    
     this.getHora()
+    this.getUsuarios()
   }
+  getUsuarios() {
+    this.usuariosService.getUsuarios().subscribe((res) => {
+        this.usuariosService.users = res;
+      }); 
+  } 
   getHorarios() {
     this.horariosService.getHorarios().subscribe((res) => {
         this.horariosService.horarios = res;
